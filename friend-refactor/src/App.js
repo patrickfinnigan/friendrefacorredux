f9b67c70c0379709a8d4eadb1e563003aae47e8e
patrickfinnigan <patrickfinnigan94@gmail.com>
2019-05-23 19:46:47 -0700

    the videos are not very informative

---
+++ friend-refactor/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import friends from "./friends.json";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    friends
+  };
+
+  removeFriend = id => {
+    console.log(id);
+    const friends = this.state.friends.filter(friend => friend.id !== id)
+    this.setState({ friends })
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <h1 className="title">Friends List</h1>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
